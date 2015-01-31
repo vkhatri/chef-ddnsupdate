39695cad3afa64894a050bc129cd12322bf71474
Virender Khatri <vir.khatri@gmail.com>
2015-01-31 22:24:44 +0530

    possible fix for some scenario for missing node.domain

--- attributes/host.rb
+++ attributes/host.rb
@@ -5,8 +5,7 @@ default['ddnsupdate']['host']['cron']['minute'] = '*/15'
 default['ddnsupdate']['host']['cron']['hour'] = '*'
 default['ddnsupdate']['host']['cron']['user'] = 'root'
 default['ddnsupdate']['host']['cron']['action'] = 'create'
-
-default['ddnsupdate']['host']['zone'] = node['domain'] # not always set
+default['ddnsupdate']['host']['zone'] = node.attribute?('domain') ? node['domain'] : nil # not always set
 default['ddnsupdate']['host']['auto_fqdn_zone'] = true
 default['ddnsupdate']['host']['reverse_zone'] = nil
 default['ddnsupdate']['host']['config'] = '/etc/nsupdate' # ddns host RR file
