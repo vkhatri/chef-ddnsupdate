15d06d0bd781b3297703c5ef94ef021c3964f3d6
Bradley Corner <bcorner@fanatics.com>
2016-01-14 11:48:11 -0500

    Incremented the version so that it will get pushed to supermarket.chef.io

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'vir.khatri@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures nsupdate to manage host dynamic dns and other RR'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.8'
+version '0.1.9'
 
 depends 'cron', '>= 1.2.0'
 
